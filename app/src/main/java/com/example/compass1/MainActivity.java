8e4dac6141a1885ce28857b542a59e8b3b78e94d
chopetintin <antonio020697@gmail.com>
2020-02-09 13:36:06 +0000

    Changed image from drawable folder to higher resolution folder

--- app/src/main/java/com/example/compass1/MainActivity.java
+++ app/src/main/java/com/example/compass1/MainActivity.java
@@ -31,6 +31,7 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
 import java.io.File;
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
@@ -57,7 +58,7 @@ public class MainActivity extends Activity implements SensorEventListener {
     private Uri fileUri;
     public static final int MEDIA_TYPE_IMAGE = 1;
 
-    public void TakePhoto(View view){
+    public void TakePhoto(View view) {
 
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         fileUri = getOutputMediaFileUri(MEDIA_TYPE_IMAGE);
@@ -128,150 +129,151 @@ public class MainActivity extends Activity implements SensorEventListener {
 
     }
 
-                private void initialization () {
-                    //TODO Auto-generated method stub
-                    //TextView initialization
-                    imageView2 = (ImageView) findViewById(R.id.imageView2);
-                    tv = (TextView) findViewById(R.id.tvDirection);
-                    grados = (TextView) findViewById(R.id.degreeview);
-                    tv.setText("Direction");
+    private void initialization() {
+        //TODO Auto-generated method stub
+        //TextView initialization
+        imageView2 = (ImageView) findViewById(R.id.imageView2);
+        tv = (TextView) findViewById(R.id.tvDirection);
+        grados = (TextView) findViewById(R.id.degreeview);
+        tv.setText("Direction");
 
 
-                    //sensors called when the activity starts
-                    sm = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-                    mSensor = sm.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
-                    aSensor = sm.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        //sensors called when the activity starts
+        sm = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mSensor = sm.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+        aSensor = sm.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
 
 
-                }
-    private void configureNextButton(){
+    }
+
+    private void configureNextButton() {
 
         button2 = (Button) findViewById(R.id.button2);
-        button2.setOnClickListener(new View.OnClickListener(){
+        button2.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view){
+            public void onClick(View view) {
                 startActivity(new Intent(MainActivity.this, BatteryActivity.class));
             }
         });
     }
 
-                private void calculateOrientation () {
-                    //TODO Auto-generated method stub
-                    float[] values = new float[3];
-                    float[] R = new float[9];
+    private void calculateOrientation() {
+        //TODO Auto-generated method stub
+        float[] values = new float[3];
+        float[] R = new float[9];
 
-                    SensorManager.getRotationMatrix(R, null, accelerometerValues, magneticFieldValues);
-                    SensorManager.getOrientation(R, values);
+        SensorManager.getRotationMatrix(R, null, accelerometerValues, magneticFieldValues);
+        SensorManager.getOrientation(R, values);
 
-                    float degree = (float) Math.toDegrees(values[0]);
-                    values[1] = (float) Math.toDegrees(values[1]);
-                    values[2] = (float) Math.toDegrees(values[2]);
-                    Log.d("CREATION", "calculateOrientation is being executed");
-                    grados.setText(Float.toString(degree));
+        float degree = (float) Math.toDegrees(values[0]);
+        values[1] = (float) Math.toDegrees(values[1]);
+        values[2] = (float) Math.toDegrees(values[2]);
+        Log.d("CREATION", "calculateOrientation is being executed");
+        grados.setText(Float.toString(degree));
 
-                    if (degree >= -5 && degree < 5) {
-                        tv.setText("North");
-                    } else if (degree >= 5 && degree < 40) {
-                        tv.setText("North North East");
-                    } else if (degree >= 40 && degree < 50) {
-                        tv.setText("North East");
-                    } else if (degree >= 50 && degree < 85) {
-                        tv.setText("East North East");
-                    } else if (degree >= 85 && degree < 95) {
-                        tv.setText("East");
-                    } else if (degree >= 95 && degree < 130) {
-                        tv.setText("East South East");
-                    } else if (degree >= 130 && degree < 140) {
-                        tv.setText("South East");
-                    } else if (degree >= 140 && degree < 175) {
-                        tv.setText("South South East");
-                    } else if ((degree >= 175 && degree <= 180) || (degree >= -180 && degree < -175)) {
-                        tv.setText("South");
-                    } else if (degree >= -175 && degree < -140) {
-                        tv.setText("South South West");
-                    } else if (degree >= -140 && degree < -130) {
-                        tv.setText("South West");
-                    } else if (degree >= -130 && degree < -95) {
-                        tv.setText("West South West");
-                    } else if (degree >= -95 && degree < -85) {
-                        tv.setText("West");
-                    } else if (degree >= -85 && degree < -50) {
-                        tv.setText("West North West");
-                    } else if (degree >= -50 && degree < -40) {
-                        tv.setText("North West");
-                    } else if (degree >= -40 && degree < -50) {
-                        tv.setText("North North West");
-                    }
-
-                    RotateAnimation ra = new RotateAnimation(
-                            -degree,
-                            -degree,
-                            Animation.RELATIVE_TO_SELF, 0.5f,
-                            Animation.RELATIVE_TO_SELF,
-                            0.5f);
-                    ra.setDuration(500);
-                    ra.setFillAfter(true);
-                    imageView2.startAnimation(ra);
-                }
-
-
-                @Override
-                protected void onPause () {
-                    super.onPause();
-                    //Unregister sensor manager when activity is not visible
-                    sm.unregisterListener(this);
-                    Log.d("PAUSE", "onPause is being executed");
-
-                }
-
-                @Override
-                protected void onResume () {
-                    super.onResume();
-                    //enable sensor manager when activity returns
-                    sm.registerListener(this, mSensor, sm.SENSOR_DELAY_GAME);
-                    sm.registerListener(this, aSensor, sm.SENSOR_DELAY_GAME);
-                    Log.d("RESUME", "onResume is being executed");
-
-
-                }
-
-                @Override
-                public void onSensorChanged (SensorEvent event){
-
-                    //read sensor value from SensorEvent
-
-
-                    Log.d("V", "ValuesPre" + Arrays.toString(event.values));
-
-                    //calculate the total magnetic field
-                    //h = Math.sqrt(event.values[0] * event.values[0] + event.values[1] * event.values[1] + event.values[2] * event.values[2]);
-                    final float alpha = 0.97f;
-                    if (event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) {
-                        //low pass filter
-                        magneticFieldValues[0] = alpha*magneticFieldValues[0] + (1-alpha)*event.values[0];
-                        magneticFieldValues[1] = alpha*magneticFieldValues[1] + (1-alpha)*event.values[1];
-                        magneticFieldValues[2] = alpha*magneticFieldValues[2] + (1-alpha)*event.values[2];
-
-
-                    }
-                    if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
-                        //low pass filter
-                        accelerometerValues[0] = alpha*accelerometerValues[0] + (1-alpha)*event.values[0];
-                        accelerometerValues[1] = alpha*accelerometerValues[1] + (1-alpha)*event.values[1];
-                        accelerometerValues[2] = alpha*accelerometerValues[2] + (1-alpha)*event.values[2];
-                    }
-
-
-                    calculateOrientation();
-
-                    Log.d("V", "Values" + Arrays.toString(event.values));
-                    Log.d("M", "Magnetic:" + Arrays.toString(magneticFieldValues));
-                    Log.d("A", "Accelerometer:" + Arrays.toString(accelerometerValues));
-                }
-
-
-                @Override
-                public void onAccuracyChanged (Sensor sensor,int accuracy){
-
-                }
+        if (degree >= -5 && degree < 5) {
+            tv.setText("North");
+        } else if (degree >= 5 && degree < 40) {
+            tv.setText("North North East");
+        } else if (degree >= 40 && degree < 50) {
+            tv.setText("North East");
+        } else if (degree >= 50 && degree < 85) {
+            tv.setText("East North East");
+        } else if (degree >= 85 && degree < 95) {
+            tv.setText("East");
+        } else if (degree >= 95 && degree < 130) {
+            tv.setText("East South East");
+        } else if (degree >= 130 && degree < 140) {
+            tv.setText("South East");
+        } else if (degree >= 140 && degree < 175) {
+            tv.setText("South South East");
+        } else if ((degree >= 175 && degree <= 180) || (degree >= -180 && degree < -175)) {
+            tv.setText("South");
+        } else if (degree >= -175 && degree < -140) {
+            tv.setText("South South West");
+        } else if (degree >= -140 && degree < -130) {
+            tv.setText("South West");
+        } else if (degree >= -130 && degree < -95) {
+            tv.setText("West South West");
+        } else if (degree >= -95 && degree < -85) {
+            tv.setText("West");
+        } else if (degree >= -85 && degree < -50) {
+            tv.setText("West North West");
+        } else if (degree >= -50 && degree < -40) {
+            tv.setText("North West");
+        } else if (degree >= -40 && degree < -50) {
+            tv.setText("North North West");
         }
+
+        RotateAnimation ra = new RotateAnimation(
+                -degree,
+                -degree,
+                Animation.RELATIVE_TO_SELF, 0.5f,
+                Animation.RELATIVE_TO_SELF,
+                0.5f);
+        ra.setDuration(500);
+        ra.setFillAfter(true);
+        imageView2.startAnimation(ra);
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //Unregister sensor manager when activity is not visible
+        sm.unregisterListener(this);
+        Log.d("PAUSE", "onPause is being executed");
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //enable sensor manager when activity returns
+        sm.registerListener(this, mSensor, sm.SENSOR_DELAY_GAME);
+        sm.registerListener(this, aSensor, sm.SENSOR_DELAY_GAME);
+        Log.d("RESUME", "onResume is being executed");
+
+
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+        //read sensor value from SensorEvent
+
+
+        Log.d("V", "ValuesPre" + Arrays.toString(event.values));
+
+        //calculate the total magnetic field
+        //h = Math.sqrt(event.values[0] * event.values[0] + event.values[1] * event.values[1] + event.values[2] * event.values[2]);
+        final float alpha = 0.97f;
+        if (event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) {
+            //low pass filter
+            magneticFieldValues[0] = alpha * magneticFieldValues[0] + (1 - alpha) * event.values[0];
+            magneticFieldValues[1] = alpha * magneticFieldValues[1] + (1 - alpha) * event.values[1];
+            magneticFieldValues[2] = alpha * magneticFieldValues[2] + (1 - alpha) * event.values[2];
+
+
+        }
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            //low pass filter
+            accelerometerValues[0] = alpha * accelerometerValues[0] + (1 - alpha) * event.values[0];
+            accelerometerValues[1] = alpha * accelerometerValues[1] + (1 - alpha) * event.values[1];
+            accelerometerValues[2] = alpha * accelerometerValues[2] + (1 - alpha) * event.values[2];
+        }
+
+
+        calculateOrientation();
+
+        Log.d("V", "Values" + Arrays.toString(event.values));
+        Log.d("M", "Magnetic:" + Arrays.toString(magneticFieldValues));
+        Log.d("A", "Accelerometer:" + Arrays.toString(accelerometerValues));
+    }
+
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+}
