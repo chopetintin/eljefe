8cc93d811fb175fc7f15147c7fb0c62b7f6ea8f7
chopetintin <antonio020697@gmail.com>
2020-02-12 23:18:17 +0000

    Refactored StepCounterFragment.

--- app/src/main/java/com/example/compass1/steps/StepCounterFragment.java
+++ app/src/main/java/com/example/compass1/steps/StepCounterFragment.java
@@ -23,9 +23,9 @@ public class StepCounterFragment extends Fragment implements SensorEventListener
 
     private SensorManager mSensorManager;
     private Sensor mstep;
-    float subtractedStepValue = 0;
+    float subtractedStepValue;
     float rawStepValue;
-    Float lastStepValue = null;
+    float lastStepValue;
     TextView step;
     TextView Step;
     Button reset;
@@ -43,13 +43,22 @@ public class StepCounterFragment extends Fragment implements SensorEventListener
         mstep = mSensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
         step = view.findViewById(R.id.test2);
         Step = view.findViewById(R.id.steptext);
+        //create the shared preferences
         pref = getActivity().getPreferences(Context.MODE_PRIVATE);
+        //fresh start, so set value to 0
+        subtractedStepValue = pref.getFloat("subtractedStepValue", 0);
+        //cannot assign null to a primitive, so have to give it an impossible value instead
+        lastStepValue = pref.getFloat("lastStepValue", -1);
         reset = view.findViewById(R.id.button);
         reset.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 lastStepValue = rawStepValue;
+                //calling the method that saves the last step
+                saveStepValue("lastStepValue", lastStepValue);
                 subtractedStepValue = 0;
+                //calling the method that saves the whole step
+                saveStepValue("subtractedStepValue", subtractedStepValue);
                 step.setText(Float.toString(subtractedStepValue));
             }
         });
@@ -59,11 +68,13 @@ public class StepCounterFragment extends Fragment implements SensorEventListener
     @Override
     public void onSensorChanged(final SensorEvent event) {
         if (event.sensor.getType() == Sensor.TYPE_STEP_COUNTER) {
-            if(lastStepValue == null){
+            if(lastStepValue == -1){
                 lastStepValue = event.values[0];
+                saveStepValue("lastStepValue", lastStepValue);
             }
             rawStepValue = event.values[0];
             subtractedStepValue = event.values[0] - lastStepValue;
+            saveStepValue("subtractedStepValue", subtractedStepValue);
             step.setText(Float.toString(subtractedStepValue));
         }
     }
@@ -102,5 +113,12 @@ public class StepCounterFragment extends Fragment implements SensorEventListener
     @Override
     public void onPause() {
         super.onPause();
+    }
+
+    private void saveStepValue(String key, Float value){
+        //Adding value into shared preferences, this is the actual steps value and commit it, which is sending it
+        SharedPreferences.Editor editor = pref.edit();
+        editor.putFloat(key, value);
+        editor.apply();
     }
 }
